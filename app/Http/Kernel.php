83dbaa2114d60c21b6b2a55d0710be56bb2909c2
haden96 <charkiewiczdaniel@gmail.com>
2020-07-03 16:29:52 +0200

    Added behav testing

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,7 +44,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\Logging::class,
             'httpHeaders:Come Work for US',
-            'auth:api',
+            // 'auth:api',
         ],
     ];
 
